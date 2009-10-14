5acbf2eeeb9a41dc5040e983944f48eb33fd0c2d
Tom Preston-Werner <tom@mojombo.com>
2009-10-13 19:39:46 -0700

    always log successful proxy connections

--- lib/proxymachine/client_connection.rb
+++ lib/proxymachine/client_connection.rb
@@ -55,9 +55,7 @@ module EventMachine
       def try_server_connect(host, port)
         @server_side = ServerConnection.request(host, port, self)
         proxy_incoming_to(@server_side, 10240)
-        if @tries > 0
-          puts "Successful connection."
-        end
+        puts "Successful connection to #{host}:#{port}."
         true
       rescue => e
         if @tries < 10
