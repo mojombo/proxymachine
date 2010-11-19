c1e5aaf161698d8e4c74038ec7b74821c3a20baf
Michael Neale <michaelneale@evenstar.local>
2010-11-19 17:54:45 +1100

    Now removes bad back ends

--- examples/load_balancer.rb
+++ examples/load_balancer.rb
@@ -1,34 +1,39 @@
-require 'memcache'
 require 'json'
 
-# MemCache.new 'localhost:11211', :namespace => 'my_namespace'
 # match server cookie, signon cookie?
 # if server cookie found, connect (deal with error)
 # if no cookie, lookup backends, apply statistical weight...
 
-@log = Logger.new(STDOUT)
+
 
 class BeeRouter
 
-  def get_backend host
-        backends = {"1.smeg.com" => ["www.unsw.edu.au:80", "news.com:80", "news.com:80"], "2.smeg.com" => "www.news.com:80"}
-        be = backends[host]
+  @backends = {"1.smeg.com" => ["localhost:8081", "localhost:8080"], "2.smeg.com" => "localhost:8080"}
+  
+  def self.get_backend host
+
+    be = @backends[host]
     if be then
       if be.class == String then
         { :remote => be }
       else
-        puts "hey 1"
         {:remote => be[rand(be.size)]}
       end
     else
       { :noop => true }
     end
   end
+
+  def self.remove_backend host
+    @backends.each do |k,v|
+      if v.instance_of?(Array) and v.size > 1
+        v.delete(host)
+      end
+    end
+  end
   
 end
 
-
-@bee_router = BeeRouter.new
 
 proxy do |data|
   # p data
@@ -38,14 +43,20 @@ proxy do |data|
   #puts "MATCH: " + $1
  # { :remote => "www.unsw.edu.au:80" }
 
-  if data =~ %r{^Host\:(.*)}
+  if data =~ %r{^Host:(.*)}
     name = $1
-    @log.info( "Host name: #{name.strip == '2.smeg.com'}" )
-    @bee_router.get_backend name.strip
+    BeeRouter.get_backend name.strip
   else
     { :noop => true }
   end
- 
+
+end
+
+
+proxy_connect_error do |remote|
+  puts "OHNOES! error connecting to #{remote}"
+  puts "Now removing it"
+  BeeRouter.remove_backend(remote)
 end
 
 
